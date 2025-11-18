5f6d8040356c78116085d78930765c1198d90344
Yimin2 <gownfla14@naver.com>
2025-11-18 14:15:29 +0900

    feat: 프로젝트 초기 설정 완료
    - Spring Boot 기반의 프로젝트 구조 생성
    - Gradle 설정 추가 및 주요 종속성 정의
    - 기본 실행 파일 및 테스트 파일 추가
    - .gitignore 및 .gitattributes 파일 설정
    - 프로젝트 빌드 및 실행에 필요한 Gradle Wrapper 추가

---
+++ src/main/java/com/ll/todoappclone/TodoAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.ll.todoappclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoAppCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/todoappclone/TodoAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.todoappclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
